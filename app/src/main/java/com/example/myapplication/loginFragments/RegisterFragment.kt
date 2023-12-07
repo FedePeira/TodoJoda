ac1990a020c8e2ed9234ca68e336338b39f34ef1
Federico Peirano <fedepr2345@gmail.com>
2023-12-07 19:06:34 -0300

    V1.0.3 Category Page - Falta customizar bien las imagenes y cambio de palabras

--- app/src/main/java/com/example/myapplication/loginFragments/RegisterFragment.kt
+++ app/src/main/java/com/example/myapplication/loginFragments/RegisterFragment.kt
@@ -1,10 +1,15 @@
 package com.example.myapplication.loginFragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.myapplication.LoginActivity
 import com.example.myapplication.R
 
 class RegisterFragment: Fragment() {
@@ -14,7 +19,21 @@ class RegisterFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        val v = inflater.inflate(R.layout.fragment_register, container, false)
+        val button = v.findViewById<Button>(R.id.mainButton)
+        button.setOnClickListener {
+            val editTextname = v.findViewById<EditText>(R.id.editTextName)
+            val name = editTextname.text.toString()
+            if (name.isEmpty()) {
+                editTextname.setError("Este campo es obligatorio")
+            } else {
+                val intent = Intent(requireContext(), LoginActivity::class.java)
+                intent.putExtra("name", name)
+                startActivity(intent)
+            }
+            findNavController().navigate(R.id.action_registerFragment_to_categoriesFragment)
+        }
+        return v
     }
 
 }
