a6ea58f0fd9dca5ffb579c71b646eff437d81679
Federico Peirano <fedepr2345@gmail.com>
2023-12-08 23:22:02 -0300

    V1.0.4 Boliche Page - Arreglo de algunas cosas de category, agregado del nuevo fragment, arreglo de id y arreglo de nombres strings.

--- app/src/main/java/com/example/myapplication/loginFragments/BolicheFragment.kt
+++ app/src/main/java/com/example/myapplication/loginFragments/BolicheFragment.kt
@@ -5,24 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.myapplication.R
 
-class CategoriesFragment: Fragment() {
+class BolicheFragment: Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-
-        val v = inflater.inflate(R.layout.fragment_categories, container, false)
-        val button = v.findViewById<AppCompatButton>(R.id.categoryBoliche)
-        button.setOnClickListener {
-            findNavController().navigate(R.id.action_mainPageFragment_to_registerFragment)
-        }
-        return v
+        return  inflater.inflate(R.layout.fragment_boliche, container, false)
     }
 }
